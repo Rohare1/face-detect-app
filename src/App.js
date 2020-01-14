cec7ef3d5e25af78700f095785b5e0f1c97dc243
Ryan o'Hare <ryan.1.ohare@gmail.com>
2020-01-14 17:48:52 +0000

    Add background image and Rank component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
 import './App.css';
 
-function App() {
-	return (
-		<div className="App">
-			<Navigation />
-			<Logo />
-			<ImageLinkForm /> 
-			{/* <FaceRecognition /> */}
-		</div>
-	);
+const particleOptions = {
+	particles: {
+		number: {
+			value: 80,
+			density: {
+				enable: true,
+				value_area: 800
+			}
+		}
+	}
+};
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<Particles className="particles" params={particleOptions} />
+				<Navigation />
+				<Logo />
+				<Rank />
+				<ImageLinkForm />
+				{/* <FaceRecognition /> */}
+			</div>
+		);
+	}
 }
 
 export default App;
- 
